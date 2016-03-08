83c8bd65cdd06cdd018909f460ad847901c44c91
Max Kreminski <maxkreminski@gmail.com>
2016-03-08 07:58:46 -0800

    Bump dependency versions

--- project.clj
+++ project.clj
@@ -1,13 +1,13 @@
 (defproject stepwise "0.0-SNAPSHOT"
   :dependencies
-  [[org.clojure/clojure "1.6.0"]
-   [org.clojure/clojurescript "0.0-3115"]
+  [[org.clojure/clojure "1.8.0"]
+   [org.clojure/clojurescript "1.7.228"]
    [org.omcljs/om "0.8.8"]
-   [prismatic/om-tools "0.3.11"]
-   [mkremins/xyzzy "0.3.3"]]
+   [prismatic/om-tools "0.3.12"]
+   [mkremins/xyzzy "0.3.4"]]
   
   :plugins
-  [[lein-cljsbuild "1.0.4"]]
+  [[lein-cljsbuild "1.1.3"]]
   
   :cljsbuild
   {:builds [{:source-paths ["src"]
